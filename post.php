09376d9a07292b57d7fcb3298941d9e63996abce
Godlia <52934844+Godlia@users.noreply.github.com>
2021-10-29 10:58:20 +0200

    Piechart

--- post.php
+++ post.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Takk for din stemme</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </head>
 
 <body>
@@ -29,6 +30,50 @@
     
     ?>
     <h1>Takk for din stemme</h1>
+    <?php
+
+
+ $dataPoints = array();
+ 
+  $sql = "SELECT partiNavn, stemmer FROM oversikt";
+  $resultat = $conn-> query($sql);
+    while ($row = $resultat->fetch_assoc()) {
+      $fun = array("label"=> $row['partiNavn'], "y"=> $row['stemmer']);
+      array_push($dataPoints, $fun);
+  }
+
+   //array("label"=>"Chrome", "y"=>64.02),
     
+   
+  
+ ?>
+
+
+    
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+
+
+
+
+<script>
+window.onload = function() {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "dark2",
+	animationEnabled: true,
+	title: {
+		text: "fun"
+	},
+	data: [{
+		type: "pie",
+		indexLabel: "{label} #percent%",
+		percentFormatString: "#0.##",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
 </body>
 </html>
